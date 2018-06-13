71b9984ea228b5e0fcd7b5de42039ef7bc63bcc9
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-13 13:23:56 -0300

    Ajustes no relatório de Faturamento

--- src/relatorio/faturamento/index.jsx
+++ src/relatorio/faturamento/index.jsx
@@ -1,35 +1,16 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import _ from 'lodash';
+import React from 'react';
 
 import { Content } from 'common';
 
-import { obterFaturamento } from './faturamentoActions';
 import GraficoFaturamento from './components/GraficoFaturamento';
 import GraficoFaturamentoPorServico from './components/GraficoFaturamentoPorServico';
 
-class RelatorioFaturamento extends Component {
-  componentWillMount() {
-    this.props.obterFaturamento();
-  }
-
-  render() {
-    return (
-      <Content
-        title="Faturamento"
-        breadcrumb={[{ name: 'Relatório', path: '#/' }, { name: 'Faturamento', path: '#/faturamento' }]}
-      >
-        <GraficoFaturamento data={this.props.faturamento} />
-        <GraficoFaturamentoPorServico />
-      </Content>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  faturamento: state.relatorio.faturamento,
-});
-const mapDispatchToProps = dispatch => bindActionCreators({ obterFaturamento }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(RelatorioFaturamento)
+export default props => (
+  <Content
+    title="Faturamento"
+    breadcrumb={[{ name: 'Relatório', path: '#/' }, { name: 'Faturamento', path: '#/faturamento' }]}
+  >
+    <GraficoFaturamento />
+    <GraficoFaturamentoPorServico />
+  </Content>
+);

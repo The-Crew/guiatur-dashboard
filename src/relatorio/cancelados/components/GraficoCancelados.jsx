47f9da0295e691262bde30c84098278e4186d908
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-13 13:52:18 -0300

    Ajustes no relatório de Cancelamentos

--- src/relatorio/cancelados/components/GraficoCancelados.jsx
+++ src/relatorio/cancelados/components/GraficoCancelados.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { Box, Grid, ChartBar } from 'common';
 
-export default (props) => {
-  const chartData = {
-    labels: _.keys(props.data),
-    datasets: [
-      {
-        label: 'Cancelamentos',
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: _.values(props.data),
-      }
-    ],
-  };
-  return (
-    <Grid cols="12 12 6 6">
-      <Box title="Cancelamentos" type="danger">
-        <ChartBar data={chartData} style={{ width: '100%' }} />
-      </Box>
-    </Grid>
-  )
-};
+import { obterCancelados } from '../canceladosActions';
+
+class Cancelamentos extends Component {
+  componentWillMount() {
+    this.props.obterCancelados();
+  }
+
+  render() {
+    const chartData = {
+      labels: _.keys(this.props.cancelados),
+      datasets: [
+        {
+          label: 'Cancelamentos',
+          fillColor: 'rgba(151,187,205,0.2)',
+          strokeColor: 'rgba(151,187,205,1)',
+          pointColor: 'rgba(151,187,205,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(151,187,205,1)',
+          data: _.values(this.props.cancelados),
+        }
+      ],
+    };
+    return (
+      <Grid cols="12 12 6 6">
+        <Box title="Cancelamentos" type="danger">
+          <ChartBar data={chartData} style={{ width: '100%' }} />
+        </Box>
+      </Grid>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ cancelados: state.relatorio.cancelados });
+const mapDispatchToProps = dispatch => bindActionCreators({ obterCancelados }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cancelamentos)
--- src/relatorio/cancelados/index.jsx
+++ src/relatorio/cancelados/index.jsx
@@ -1,33 +1,16 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import _ from 'lodash';
+import React from 'react';
 
 import { Content } from 'common';
 
-import { obterCancelados } from './canceladosActions';
 import GraficoCancelados from './components/GraficoCancelados';
 import GraficoCanceladosPorBairro from './components/GraficoCanceladosPorBairro';
 
-class Relatorio extends Component {
-  componentWillMount() {
-    this.props.obterCancelados();
-  }
-
-  render() {
-    return (
-      <Content
-        title="Cancelamentos"
-        breadcrumb={[{ name: 'Relatório', path: '#/' }, { name: 'Cancelamentos', path: '#/cancelamentos' }]}
-      >
-        <GraficoCancelados data={this.props.cancelados} />
-        <GraficoCanceladosPorBairro />
-      </Content>
-    );
-  }
-}
-
-const mapStateToProps = state => ({ cancelados: state.relatorio.cancelados });
-const mapDispatchToProps = dispatch => bindActionCreators({ obterCancelados }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Relatorio)
+export default props => (
+  <Content
+    title="Cancelamentos"
+    breadcrumb={[{ name: 'Relatório', path: '#/' }, { name: 'Cancelamentos', path: '#/cancelamentos' }]}
+  >
+    <GraficoCancelados />
+    <GraficoCanceladosPorBairro />
+  </Content>
+);

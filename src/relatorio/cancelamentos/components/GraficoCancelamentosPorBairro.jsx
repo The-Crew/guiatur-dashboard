77deb29e1f0585c6df19a1335d38ed4c7caed19c
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-14 09:21:11 -0300

    Ajuste no gráfico de cancelamentos por bairro

--- src/relatorio/cancelamentos/components/GraficoCancelamentosPorBairro.jsx
+++ src/relatorio/cancelamentos/components/GraficoCancelamentosPorBairro.jsx
@@ -1,50 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { Box, Grid, ChartBar } from 'common';
 
+import { obterListaDeBairros } from '../../relatorioActions';
 import { obterCancelamentosPorBairro } from '../cancelamentosActions';
 
-const CancelamentosPorServico = (props) => {
-  const chartData = {
-    labels: _.keys(props.cancelamentosPorBairro),
-    datasets: [
-      {
-        label: 'Cancelamentos por bairro',
-        fillColor: 'rgba(151,187,205,0.2)',
-        strokeColor: 'rgba(151,187,205,1)',
-        pointColor: 'rgba(151,187,205,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(151,187,205,1)',
-        data: _.values(props.cancelamentosPorBairro),
-      },
-    ],
-  };
+class CancelamentosPorServico extends Component {
+  componentWillMount() {
+    this.props.obterListaDeBairros();
+  }
 
-  return (
-    <Grid cols="12 12 6 6">
-      <Box title="Cancelamentos por bairro" type="warning">
-        <ChartBar data={chartData} style={{ width: '100%' }} />
-        <select onChange={e => props.obterCancelamentosPorBairro(e.target.value)}>
-          <option value="">Selecione um serviço</option>
-          {props.listaBairros.map(bairro => (
-            <option key={bairro.Descricao} value={bairro.Descricao}>
-              {bairro.Descricao}
-            </option>
-          ))}
-        </select>
-      </Box>
-    </Grid>
-  );
-};
+  componentWillUnmount() {
+    this.props.obterCancelamentosPorBairro('');
+  }
+
+  render() {
+    const chartData = {
+      labels: _.keys(this.props.cancelamentosPorBairro),
+      datasets: [
+        {
+          label: 'Cancelamentos por bairro',
+          fillColor: 'rgba(151,187,205,0.2)',
+          strokeColor: 'rgba(151,187,205,1)',
+          pointColor: 'rgba(151,187,205,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(151,187,205,1)',
+          data: _.values(this.props.cancelamentosPorBairro),
+        },
+      ],
+    };
+
+    return (
+      <Grid cols="12 12 6 6">
+        <Box title="Cancelamentos por bairro" type="warning">
+          <ChartBar data={chartData} style={{ width: '100%' }} />
+          <select onChange={e => this.props.obterCancelamentosPorBairro(e.target.value)}>
+            <option value="">Selecione um serviço</option>
+            {this.props.listaBairros.map(bairro => (
+              <option key={bairro.Bairro} value={bairro.Bairro}>
+                {bairro.Bairro}
+              </option>
+            ))}
+          </select>
+        </Box>
+      </Grid>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   listaBairros: state.relatorio.listaBairros,
   cancelamentosPorBairro: state.relatorio.cancelamentosPorBairro,
 });
-const mapDispatchToProps = dispatch => bindActionCreators({ obterCancelamentosPorBairro }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ obterListaDeBairros, obterCancelamentosPorBairro }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(CancelamentosPorServico);
